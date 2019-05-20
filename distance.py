7a587c3ed8f96406b682de6a5e56f38c7ce20174
Gbenga Owoeye <gbengulo005@gmail.com>
2019-05-20 09:26:02 -0500

    Add files via upload

---
+++ distance.py
@@ -0,0 +1,31 @@
+class Point:
+
+    def __init__(self, initX, initY):
+
+        self.x = initX
+        self.y = initY
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+    def distanceFromOrigin(self):
+        return ((self.x ** 2) + (self.y ** 2)) ** 0.5
+
+    def __str__(self):
+        return'Point({}, {})'.format(self.x, self.y)
+# in python, if you really want to see the points here, you need to use the str function.
+
+    def distance(self, point2):
+        xdiff = point2.getX()-self.getX()
+        ydiff = point2.getY()-self.getY()
+
+        dist = math.sqrt(xdiff**2 + ydiff**2)
+        return dist
+
+p = Point(4,3)
+q = Point(0,0)
+print(p)
+print(q)
